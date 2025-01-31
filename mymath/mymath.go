7c9a25a9ae64882da9f4c4d31b487e29c0012d51
Артем Авдеев <astranotus8_gmail.com>
2025-01-31 19:05:09 +0400

    fix

---
+++ mymath/mymath.go
@@ -0,0 +1,31 @@
+package mymath
+
+import "math"
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
